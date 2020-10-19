6e420f1710a8ef36fc284828086a9c0140d51e02
Berkley Brown <berkleydeane@gmail.com>
2020-10-19 14:54:04 -0500

    completed search and clear state of search bar, beginning css

--- src/App.js
+++ src/App.js
@@ -9,13 +9,14 @@ const App = () => {
  
  const [recipes, setRecipes] = useState([]);
  const [search, setSearch] = useState('');
+ const [query, setQuery] = useState('chicken')
 
  useEffect( () => {
    getRecipes();
- }, []);
+ }, [query]);
 
  const getRecipes = async () => {
-     const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+     const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
      )
      const data = await response.json();
      setRecipes(data.hits)
@@ -24,12 +25,17 @@ const App = () => {
 
     const updateSearch = e => {
       setSearch(e.target.value)
-      console.log(search)
+    }
+
+    const getSearch = e => {
+      e.preventDefault();
+      setQuery(search)
+      setSearch('')
     }
 
   return (
     <div className="App">
-      <form className="search-form">
+      <form onSubmit={getSearch} className="search-form">
         <input className="search-bar" type="text" value={search} onChange={updateSearch}/>
   <button className="search-button" type="submit">Search</button>
        </form>
@@ -39,6 +45,7 @@ const App = () => {
          title={recipe.recipe.label} 
          calories={recipe.recipe.calories} 
          image={recipe.recipe.image}
+         ingredients={recipe.recipe.ingredients}
          />
        ))}
     </div>
