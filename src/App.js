8c4261c27cad460a425a7db901e5d34b57242017
github username <github email address>
2020-10-19 10:18:31 -0500

    started form component

--- src/App.js
+++ src/App.js
@@ -7,11 +7,15 @@ const App = () => {
   const APP_ID ='d59b829c'
   const APP_KEY = 'adb5ada24faea25208f644371c34d87a	'
 
-  const exampleReg = "https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}"
+  const exampleReg = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
 
   return (
     <div className="App">
-      <h1>Hello mortal!</h1>
+      <form className="search-form">
+        <input className="search-bar" type="text"/>
+        <button className="search-button" type="submit">Search</button>
+       
+      </form>
     </div>
   );
 }
