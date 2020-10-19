e251020fbd4f80cfec2e63e379730c9b77932cd3
github username <github email address>
2020-10-19 10:30:21 -0500

    added useEffect function

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import React from 'react';
-
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 const App = () => {
 
   const APP_ID ='d59b829c'
   const APP_KEY = 'adb5ada24faea25208f644371c34d87a	'
+ const exampleReg = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
 
-  const exampleReg = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+ const [counter, setCounter] = useState(0);
+ 
+ useEffect(() => {
+   console.log('Effect is run')
+ });
+
 
   return (
     <div className="App">
       <form className="search-form">
         <input className="search-bar" type="text"/>
-        <button className="search-button" type="submit">Search</button>
-       
-      </form>
+  <button className="search-button" type="submit">Search</button>
+       </form>
+  <h1 onClick={() => setCounter(counter + 1)}>{counter}</h1>
     </div>
   );
 }
